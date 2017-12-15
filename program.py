8175c59202c06d8670748d5e596eba5ce22f6d04
wesley <wesleyyw8@gmail.com>
2017-12-15 22:21:27 +0000

    a

--- program.py
+++ program.py
@@ -1,20 +1,7 @@
-
-iris_dic = {}
-
-for l in open('iris.data.txt','r'):
-    sepalL, sepalW, petalL, petalW, classN = l.strip('\n').split(',')
-    if classN in iris_dic.keys():
-        iris_dic[classN].append([sepalL, sepalW, petalL, petalW])
-    else:
-        iris_dic[classN] = [ [sepalL, sepalW, petalL, petalW] ]
-
-print iris_dic
-#print sum(iris_dic['Iris-virginica'][])
-
-
-#print sum([float(x[0]) for x in iris_dic['Iris-virginica']])
-
-#for k in iris_dic:
-#    print k
-#    print iris_dic[k]
-#    print '----------------'
+N=5
+f=open("pollution_us_2000_2016.csv")
+for i in range(N):
+    if i > 0:
+      line=f.next().strip().split(',')
+      print line[2]
+f.close()
