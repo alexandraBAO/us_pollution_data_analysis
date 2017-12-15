eb4f30c82ef3c2b4b813dbdc5f1507c210879e98
wesley <wesleyyw8@gmail.com>
2017-12-15 23:25:24 +0000

    testing the class

--- program.py
+++ program.py
@@ -1,7 +1,22 @@
+class MyClass:
+  def __init__(self, state, county, city, dateLocal, no2Mean, o3Mean, so2Mean, coMean):
+    self.state = state
+    self.county = county
+    self.city = city
+    self.dateLocal = dateLocal
+    self.no2Mean = no2Mean
+    self.o3Mean = o3Mean
+    self.so2Mean = so2Mean
+    self.coMean = coMean
+
+results = []
+
 N=5
 f=open("pollution_us_2000_2016.csv")
 for i in range(N):
-    if i > 0:
-      line=f.next().strip().split(',')
-      print line[2]
+  line=f.next().strip().split(',')
+  if (i > 0):
+    results.append(MyClass(line[5], line[6], line[7], line[8], line[9], line[15], line[21], line[27]))
 f.close()
+
+print results[3].county
