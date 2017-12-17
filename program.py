f7880bd729d1fbdba149d1cacb214e1740bfa1bd
Sui Cen Teng <xandra.tsc@gmail.com>
2017-12-17 17:22:35 +0000

    codes working

--- program.py
+++ program.py
@@ -1,29 +1,10 @@
 import pandas as pd
 import dateutil
-import datetime
 
-class MyClass:
-  def __init__(self, state, county, city, dateLocal, no2Mean, o3Mean, so2Mean, coMean):
-    self.state = state
-    self.county = county
-    self.city = city
-    self.dateLocal = dateLocal
-    self.no2Mean = no2Mean
-    self.o3Mean = o3Mean
-    self.so2Mean = so2Mean
-    self.coMean = coMean
+data = pd.read_csv('pollution_us_2000_2016.csv')
 
-results = []
+data['year'] = data['Date Local'].apply(dateutil.parser.parse, dayfirst=True)
 
-data = pd.read_csv('pollution_us_2000_2016.csv', nrows=20)
-
-data['date'] = data['Date Local'].apply(dateutil.parser.parse, dayfirst=True)
-
-# lines = data['NO2 Mean'].count()
-# no2MeanAverage = data['NO2 Mean'].sum() / lines
-
-test = data.groupby(data['date'].map(lambda x: x.year))[['NO2 Mean']].mean()
-print test
-
-
-# print 'The average consum of NO2 Mean from ' + str(min(data['date'])) + ' to ' + str(max(data['date'])) + ' is: ' + str(no2MeanAverage)
+# print data.groupby(data['year'].map(lambda x: x.year))[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
+print data.groupby(data['year'].map(lambda x: x.year))[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].max()
+	
