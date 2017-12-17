899c87a302e5f836d3597e9e4d5ad8356e3b5e8e
wesley <wesleyyw8@gmail.com>
2017-12-17 20:48:04 +0000

    .

--- program.py
+++ program.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import dateutil
 
-data = pd.read_csv('pollution_us_2000_2016.csv', nrows=10)
+data = pd.read_csv('pollution_us_2000_2016.csv')
 
 data['year'] = data['Date Local'].apply(dateutil.parser.parse, dayfirst=True)
-data['city'] = data['City']
+
+data['CO Mean'] = data['CO Mean'].map(lambda x: x*1000)
+data['O3 Mean'] = data['O3 Mean'].map(lambda x: x*1000)
 
 # Average emission of pollutants per day in each year
-print data.groupby(data['year'].map(lambda x: x.year))[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
+# print data.groupby(data['year'].map(lambda x: x.year))[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
+
+
+
+print data.groupby(['State', data['year'].map(lambda x: x.year)])[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
+
+
+# print data.groupby(['State', data['year'].map(lambda x: x.year)])[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
