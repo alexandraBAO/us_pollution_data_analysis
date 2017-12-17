4b332e54fc72cb82d4b44f8b576978039f46849b
Sui Cen Teng <xandra.tsc@gmail.com>
2017-12-17 18:00:21 +0000

    file

--- program.py
+++ program.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import dateutil
 
-data = pd.read_csv('pollution_us_2000_2016.csv')
+data = pd.read_csv('pollution_us_2000_2016.csv', nrows=10)
 
 data['year'] = data['Date Local'].apply(dateutil.parser.parse, dayfirst=True)
+data['city'] = data['City']
 
-# print data.groupby(data['year'].map(lambda x: x.year))[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
-print data.groupby(data['year'].map(lambda x: x.year))[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].max()
-	
+# Average emission of pollutants per day in each year
+print data.groupby(data['year'].map(lambda x: x.year))[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
