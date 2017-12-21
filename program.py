8ad55e021ad9fbf84d3a634837de47f4c0885da3
Manoel Rebelo Abranches <manoel.abranches@walmart.com>
2017-12-20 23:23:19 -0200

    small bug fix and group by month

--- program.py
+++ program.py
@@ -8,7 +8,7 @@ data = pd.read_csv('pollution_us_2000_2016.csv')
 #get just data for year 2015
 # can extract to a function or method later
 year = 2015
-data = data.loc[(data['Date Local'] > (str(year-1) + '-12-31')) & ( data['Date Local'] < str(year + 1))]['Date Local']
+data = data.loc[(data['Date Local'] > (str(year-1) + '-12-31')) & ( data['Date Local'] < str(year + 1))]
 
 
 data['year'] = data['Date Local'].apply(dateutil.parser.parse, dayfirst=True)
@@ -21,4 +21,4 @@ data['O3 Mean'] = data['O3 Mean'].map(lambda x: x*1000)
 # print data.groupby(['State', data['year'].map(lambda x: x.year)])[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
 
 
-print data.groupby(['State', data['year'].map(lambda x: x.year)])[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
+print data.groupby(['State', data['year'].map(lambda x: x.month)])[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
