bf3a177acb1e200dab62a68b192f446cae8ac097
Manoel Rebelo Abranches <manoel.abranches@walmart.com>
2017-12-20 23:18:54 -0200

    filter data by year

--- program.py
+++ program.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import dateutil
+import re
 
-data = pd.read_csv('pollution_us_2000_2016.csv', nrows = 50)
+pattern = re.compile('2015')
+data = pd.read_csv('pollution_us_2000_2016.csv')
+
+#get just data for year 2015
+# can extract to a function or method later
+year = 2015
+data = data.loc[(data['Date Local'] > (str(year-1) + '-12-31')) & ( data['Date Local'] < str(year + 1))]['Date Local']
+
 
 data['year'] = data['Date Local'].apply(dateutil.parser.parse, dayfirst=True)
-
 data['CO Mean'] = data['CO Mean'].map(lambda x: x*1000)
 data['O3 Mean'] = data['O3 Mean'].map(lambda x: x*1000)
 
