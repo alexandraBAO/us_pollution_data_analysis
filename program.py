8805960734750e0e0c85cdf64180085b5e6d4767
wesley <wesleyyw8@gmail.com>
2017-12-22 21:00:17 +0000

    ,

--- program.py
+++ program.py
@@ -2,7 +2,8 @@ import pandas as pd
 import dateutil
 import re
 
-pattern = re.compile('2015')
+year = 2016
+pattern = re.compile(str(year))
 data = pd.read_csv('pollution_us_2000_2016.csv')
 data['year'] = data['Date Local'].apply(dateutil.parser.parse, dayfirst=True)
 data['CO Mean'] = data['CO Mean'].map(lambda x: x*1000)
@@ -10,8 +11,7 @@ data['O3 Mean'] = data['O3 Mean'].map(lambda x: x*1000)
 
 #get just data for year 2015
 # can extract to a function or method later
-year = 2015
-data_2015 = data.loc[(data['Date Local'] > (str(year-1) + '-12-31')) & ( data['Date Local'] < str(year + 1))]
+data_result = data.loc[(data['Date Local'] > (str(year-1) + '-12-31')) & ( data['Date Local'] < str(year + 1))]
 
 
 
@@ -21,16 +21,17 @@ data_2015 = data.loc[(data['Date Local'] > (str(year-1) + '-12-31')) & ( data['D
 # print data.groupby(['State', data['year'].map(lambda x: x.year)])[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
 
 
-result =  data_2015.groupby(['State', data['year'].map(lambda x: x.month)])[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
+
+result =  data_result.groupby([data['year'].map(lambda x: x.month)])[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
 result.rename(index=str, columns={"year": "month"})
+print year
 print result
 #2010 without grouping data
 
 
 #get 2010 data
-year = 2010
-data_2010 = data.loc[(data['Date Local'] > (str(year-1) + '-12-31')) & ( data['Date Local'] < str(year + 1))]
+# year_2010 = data.loc[(data['Date Local'] > (str(year-1) + '-12-31')) & ( data['Date Local'] < str(year + 1))]
 
-result =  data_2015[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
+# result =  data_2015[['NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean']].mean()
 
-print result
+# print result
